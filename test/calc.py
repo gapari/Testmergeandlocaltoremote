2fb1ec6d011d19cf6b088f4e9385fe3858aa897a
Paripelly Gangadhargit config --global user.email v-gapari@microsoft.comgit config --global user.name Paripelly <you@example.com>
2022-11-23 19:28:30 +0530

    added few lines from stash

--- test/calc.py
+++ test/calc.py
@@ -3,5 +3,13 @@ num1 = input('Enter first number:')
 num2 = input('Enter second number:')
 #calc operations
 sum = float(num1) + float(num2)
+diff = float(num1) -  float(num2)
+mul = float(num1) * float(num2)
+div = float(num1) / float(num2)
+
+
+
+
+
 # display the result 
 print('print the sum of {0} and {1} is {2}'.format(num1,num2,sum))
