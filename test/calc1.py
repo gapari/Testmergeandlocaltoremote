a9e6868063d2a77008412ed793a826b3df974847
Paripelly Gangadhargit config --global user.email v-gapari@microsoft.comgit config --global user.name Paripelly <you@example.com>
2022-11-23 23:36:11 +0530

    added new file calc1

--- test/calc1.py
+++ test/calc1.py
@@ -3,9 +3,6 @@ num1 = 6.6
 num2 = 2.2
 #calc operations
 sum = float(num1) + float(num2)
-diff = float(num1) -  float(num2)
-mul = float(num1) * float(num2)
-div = float(num1) / float(num2)
 
 # display the result 
 print('print the sum of {0} and {1} is {2}'.format(num1,num2,sum))
