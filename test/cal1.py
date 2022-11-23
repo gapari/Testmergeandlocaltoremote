05b577066475966bdb72828a6cf1f51591afa724
Paripelly Gangadhargit config --global user.email v-gapari@microsoft.comgit config --global user.name Paripelly <you@example.com>
2022-11-23 23:52:44 +0530

    added stash lines

--- test/cal1.py
+++ test/cal1.py
@@ -3,6 +3,14 @@ num1 = input('Enter first number:')
 num2 = input('Enter second number:')
 #calc operations
 sum = float(num1) + float(num2)
+diff = float(num1) -  float(num2)
+mul = float(num1) * float(num2)
+div = float(num1) / float(num2)
+
 
 # display the result 
 print('print the sum of {0} and {1} is {2}'.format(num1,num2,sum))
+print('print the diff of {0} and {1} is {2}'.format(num1,num2,diff))
+print('print the mul of {0} and {1} is {2}'.format(num1,num2,mul))
+print('print the div of {0} and {1} is {2}'.format(num1,num2,div))
+
