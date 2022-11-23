a2af16575c1a5f8fe2560159e06fe3a694f8e894
Paripelly Gangadhargit config --global user.email v-gapari@microsoft.comgit config --global user.name Paripelly <you@example.com>
2022-11-23 19:05:14 +0530

    added file calc

---
+++ test/calc.py
@@ -0,0 +1,7 @@
+#proram to manipulate two commans
+num1 = 6.6
+num2 = 2.2
+#calc operations
+sum = float(num1) + float(num2)
+# display the result 
+print('print the sum of {0} and {1} is {2}'.format(num1,num2,sum))
