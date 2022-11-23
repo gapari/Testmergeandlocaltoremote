764c9d834f5008761725cc8f19adf4d67f58ccb2
Paripelly Gangadhargit config --global user.email v-gapari@microsoft.comgit config --global user.name Paripelly <you@example.com>
2022-11-23 23:41:12 +0530

    removed added lines

--- test/calc1.py
+++ test/calc1.py
@@ -3,12 +3,7 @@ num1 = 6.6
 num2 = 2.2
 #calc operations
 sum = float(num1) + float(num2)
-diff = float(num1) -  float(num2)
-mul = float(num1) * float(num2)
-div = float(num1) / float(num2)
+
 
 # display the result 
 print('print the sum of {0} and {1} is {2}'.format(num1,num2,sum))
-print('print the diff of {0} and {1} is {2}'.format(num1,num2,diff))
-print('print the mul of {0} and {1} is {2}'.format(num1,num2,mul))
-print('print the div of {0} and {1} is {2}'.format(num1,num2,div))
